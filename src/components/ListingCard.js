c07408c09bb28b694f40ded29ad1462d8037c0d8
Joseph Guillory <jagmanx99@yahoo.com>
2022-06-29 14:57:03 -0700

    2nd commit

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import React from "react";
 
 function ListingCard({list}) {
+
+  const [liked, setLiked] = useState(false)
+
   return (
     <li className="card">
       <div className="image">
@@ -8,10 +12,10 @@ function ListingCard({list}) {
         <img src={list.image} alt={list.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {liked ? (
+          <button className="emoji-button favorite active" onClick={()=> setLiked(!liked) }>★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite" onClick={()=> setLiked(!liked) } >☆</button>
         )}
         <strong>{list.description}</strong>
         <span> · {list.location}</span>
